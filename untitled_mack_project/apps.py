4ce67f3910c68005ab24a25036f426d1b9f90f25
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2023-10-30 20:32:38 -0400

    Update django app name

--- untitled_mack_project/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class UntitledMackProjectConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'untitled_mack_project'
---
+++ untitled_mack_project_backend/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class UntitledMackProjectBackendConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'untitled_mack_project_backend'
