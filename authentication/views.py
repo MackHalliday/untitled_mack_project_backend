b80ec9a6cf7cd83645d5466e2d6c90e6332fe3c2
Mackenzie Halliday <mackenzie.halliday@gmail.com>
2023-12-08 13:07:34 -0500

    Update log, create method to handle save user info, create form to handle user info

--- authentication/views.py
+++ authentication/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
+from django.contrib import messages
 
 
 def home(request):
@@ -6,6 +8,24 @@ def home(request):
 
 
 def signup(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        first_name = request.POST.get("first_name")
+        last_name = request.POST.get("last_name")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        confirm_password = request.POST.get("confirm_password")
+
+        new_user = User.objects.create_user(username, email, password)
+        new_user.first_name = first_name
+        new_user.last_name = last_name
+
+        new_user.save()
+
+        messages.success(request, "Your account has been created.")
+
+        return redirect("signin")
+
     return render(request, "authentication/signup.html")
 
 
--- logs.md
+++ logs.md
@@ -1,6 +1,20 @@
 # 2023-12-04 
+## Overview
 - Create basic Django application
 - Create venv 
 - Create Views, Urls, and Templates for authentication app
   
-# 2023-12-05 
+# 2023-12-05
+## Overview
+- Continued building auth functionality with help from the YouTube video [How to Create a Login System in Python Using Django?](https://www.youtube.com/watch?v=1UvTNMH7zDo)
+- Started branches
+
+## Creating Signin Page 
+- Use `<form>` and `<input>` to create form that accepts first name, last name, username, email, and password 
+- Use `{% csrf_token %}` tag within form to generate a CSRF token. Token is used to protect Django application from cross-site request forgery (CSRF) attacks.
+
+
+## Contrib package
+- Django's built tooling [contrib package](https://docs.djangoproject.com/en/4.2/ref/contrib/)
+- Can return messages to user after code in View is executed with `messages.success(request, "message")`
+  
